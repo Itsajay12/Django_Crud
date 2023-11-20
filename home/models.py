065a90503046774c9e7523a271efff1992492317
Ajay v <ajayv570040@gmail.com>
2023-11-20 15:05:50 +0530

    meadia dir created

--- home/models.py
+++ home/models.py
@@ -5,3 +5,6 @@ class Login(models.Model):
     name=models.CharField(max_length=20)
     email=models.EmailField(primary_key=True)
     password=models.CharField(max_length=20)
+    profile_pic=models.ImageField(upload_to='uploads',null="No images")
+    def __str__(self) :
+        return self.name
--- home/views.py
+++ home/views.py
@@ -1,24 +1,36 @@
 from django.shortcuts import render
-from .models import*
+from .models import *
 # Create your views here.
+
+
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
+
+
 def forms(request):
-    if request.method=="POST":
-        query=Login.objects.create(name=request.POST['name'],email=request.POST['email'],password=request.POST['password'])
+    if request.method == "POST":
+        query = Login.objects.create(
+            name=request.POST['name'], email=request.POST['email'], password=request.POST['password'],profile_pic=request.FILES['file1'])
         query.save()
         return view(request)
-    return render(request,'form.html')
+    return render(request, 'form.html')
+
+
 def view(request):
-    query=Login.objects.all()
-    return render(request,'view.html',{"query":query})
-def update(request,email):
-    query=Login.objects.filter(email=email)
-    if request.method=="POST":
-        Login.objects.filter(email = email).update(name=request.POST['name'],password=request.POST['password'])
+    query = Login.objects.all()
+    return render(request, 'view.html', {"query": query})
+
+
+def update(request, email):
+    query = Login.objects.filter(email=email)
+    if request.method == "POST":
+        Login.objects.filter(email=email).update(
+            name=request.POST['name'], password=request.POST['password'])
         return index(request)
-    return render(request,'update.html',{"query":query})
-def delete(request,email):
+    return render(request, 'update.html', {"query": query})
+
+
+def delete(request, email):
     Login.objects.filter(email=email).delete()
-    
+
     return index(request)
